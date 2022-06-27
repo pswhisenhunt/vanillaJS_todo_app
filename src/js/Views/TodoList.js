fd52a24ee68529a3791625f51aca5ca8d9f584fc
Pam Whisenhunt <pam.s.whisenhunt@gmail.com>
2022-06-27 15:17:26 -0500

    removed previous app version

--- src/js/Views/TodoList.js
+++
@@ -1,8 +0,0 @@
-function renderTodos() {
-    const todoList = document.getElementById('todo-list');
-    todoList.classList.add('todo-list');
-    const todos = JSON.parse(localStorage.getItem('todos')) || [];
-    todos.forEach((todo) => {
-        todoList.appendChild(createTodoView(todo))
-    });
-}
